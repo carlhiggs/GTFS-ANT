b8f2ae1afacdcf0fc623ba0c29734dc972c29a23
Carl Higgs <carl.higgs@rmit.edu.au>
2019-07-15 16:42:56 +1000

    updated gtfs script

--- gtfs_to_psql.py
+++ gtfs_to_psql.py
@@ -12,7 +12,7 @@ import psycopg2         ,\
 from sqlalchemy import create_engine
 from StringIO import StringIO
 from zipfile import ZipFile
-from urllib import urlopen
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 cwd = os.path.dirname(sys.argv[0])
 print(cwd)
@@ -47,6 +47,7 @@ args = parser.parse_args()
 
 # connect to database
 conn = psycopg2.connect(dbname=args.db, user=args.U, password=args.w)
+conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) # <-- ADD THIS LINE
 curs = conn.cursor()
 
 engine = create_engine("postgresql://{user}:{pwd}@{host}/{db}".format(user = args.U,
@@ -64,199 +65,178 @@ It is recommended that you ensure your GTFS file names are
 For example: "gtfs_vic_ptv_20180413" follows a schema, 'gtfs_state_agency_yyyymmdd' 
 This will allow for easy storage and retrieval of multiple gtfs feed databases.
 ''')
+required_files = ['agency.txt', 'calendar.txt', 'calendar_dates.txt', 'routes.txt', 'shapes.txt', 'stop_times.txt', 'stops.txt', 'trips.txt'] 
+
+required_tables_sql = '''
+DROP DOMAIN IF EXISTS wgs84_lat CASCADE;
+CREATE DOMAIN wgs84_lat AS DOUBLE PRECISION CHECK(VALUE >= -90 AND VALUE <= 90);
+
+DROP DOMAIN IF EXISTS wgs84_lon CASCADE;
+CREATE DOMAIN wgs84_lon AS DOUBLE PRECISION CHECK(VALUE >= -180 AND VALUE <= 180);
+
+DROP DOMAIN IF EXISTS gtfstime CASCADE;
+CREATE DOMAIN gtfstime AS text CHECK(VALUE ~ '^[0-9]?[0-9]:[0-5][0-9]:[0-5][0-9]$');
+
+CREATE TABLE agency
+(
+  agency_id         text UNIQUE NULL,
+  agency_name       text NOT NULL,
+  agency_url        text NOT NULL,
+  agency_timezone   text NOT NULL,
+  agency_lang       text NULL
+);
+
+CREATE TABLE calendar
+(
+  service_id        text PRIMARY KEY,
+  monday            boolean NOT NULL,
+  tuesday           boolean NOT NULL,
+  wednesday         boolean NOT NULL,
+  thursday          boolean NOT NULL,
+  friday            boolean NOT NULL,
+  saturday          boolean NOT NULL,
+  sunday            boolean NOT NULL,
+  start_date        numeric(8) NOT NULL,
+  end_date          numeric(8) NOT NULL
+);
+
+CREATE TABLE calendar_dates
+(
+  service_id text NOT NULL,
+  date numeric(8) NOT NULL,
+  exception_type integer NOT NULL
+);
+
+CREATE TABLE routes
+(
+  route_id          text PRIMARY KEY,
+  agency_id         text NULL,
+  route_short_name  text NULL,
+  route_long_name   text NOT NULL,
+  route_type        integer NULL,
+  route_color       text NULL,
+  route_text_color  text NULL
+);
+
+CREATE TABLE shapes
+(
+  shape_id          text,
+  shape_pt_lat      wgs84_lat NOT NULL,
+  shape_pt_lon      wgs84_lon NOT NULL,
+  shape_pt_sequence integer NOT NULL,
+  shape_dist_traveled double precision NULL
+);
+
+-- CREATE TABLE stop_times
+-- (
+  -- trip_id           text NOT NULL,
+  -- arrival_time      interval NOT NULL,
+  -- departure_time    interval NOT NULL,
+  -- stop_id           text NOT NULL,
+  -- stop_sequence     integer NOT NULL,
+  -- stop_headsign     text NULL,
+  -- pickup_type       integer NULL CHECK(pickup_type >= 0 and pickup_type <=3),
+  -- drop_off_type     integer NULL CHECK(drop_off_type >= 0 and drop_off_type <=3),
+  -- shape_dist_traveled double precision NULL
+-- );
+-- Using a work around due to empty value in shape_dist_traveled field; can't be interpreted as numeric directly
+CREATE TABLE stop_times
+(
+  trip_id           text NOT NULL,
+  arrival_time      interval NOT NULL,
+  departure_time    interval NOT NULL,
+  stop_id           text NOT NULL,
+  stop_sequence     integer NOT NULL,
+  stop_headsign     text NULL,
+  pickup_type       integer NULL CHECK(pickup_type >= 0 and pickup_type <=3),
+  drop_off_type     integer NULL CHECK(drop_off_type >= 0 and drop_off_type <=3),
+  shape_dist_traveled text NULL
+);
+
+CREATE TABLE stops
+(
+  stop_id           text PRIMARY KEY,
+  stop_name         text NOT NULL,
+  stop_lat          wgs84_lat NOT NULL,
+  stop_lon          wgs84_lon NOT NULL
+);
+
+CREATE TABLE trips
+(
+  route_id          text NOT NULL,
+  service_id        text NOT NULL,
+  trip_id           text NOT NULL PRIMARY KEY,
+  shape_id          text NULL,
+  trip_headsign     text NULL,
+  direction_id      boolean NULL
+);
+'''
+
 for root, dirs, files in os.walk(args.dir):
-    for file in files:
-        if file.endswith(".zip"):
-            path = '{}/{}'.format(root,file)
-            print('\n{}'.format(path))
-            name = os.path.splitext(file)[0]            
-            with ZipFile(path) as myzip:
-                file_list = ZipFile.namelist(myzip)
-                print("Zip contents: {}".format(file_list))
-                test_contents = [x for x in ['agency.txt', 'calendar.txt', 'calendar_dates.txt', 'routes.txt', 'shapes.txt', 'stop_times.txt', 'stops.txt', 'trips.txt'] if x in file_list]
-                if (len(test_contents)!=len(file_list)):
-                    wrong_contents = [x for x in ['agency.txt', 'calendar.txt', 'calendar_dates.txt', 'routes.txt', 'shapes.txt', 'stop_times.txt', 'stops.txt', 'trips.txt'] if x not in file_list]
-                    print("The zip file appears to be missing the following required files: {}".format(wrong_content))
-                # # SQL queries
-                # sql =  = '''
-                  # CREATE DATABASE IF NOT EXISTS {}
-                 # '''.format(name)  
-                # curs.execute(sql)
-                # conn.commit()
-# with myzip.open('eggs.txt') as myfile:
-    # print(myfile.read())
-        # zipfile = ZipFile(StringIO(path))
-        # for name in myzipfile.namelist():
-          # print(name)
-     
+  for file in files:
+    if file.endswith(".zip"):
+      path = '{}/{}'.format(root,file)
+      print('\n{}'.format(path))
+      name = os.path.splitext(file)[0]            
+      with ZipFile(path) as myzip:
+        file_list = ZipFile.namelist(myzip)
+        # print("Zip contents: {}".format(file_list))
+        test_contents = [x for x in required_files if x not in file_list]
+        if (len(test_contents)!=0):
+          print("The zip file appears to be missing the following required files: {}".format(test_contents))
+          continue
+        else:
+            print("\t- Required files are present.")
+            # check if sql database exists
+            sql =  '''SELECT 1 FROM pg_catalog.pg_database WHERE datname='{}';'''.format(name)
+            curs.execute(sql)
+            if len(curs.fetchall())!=0:
+              print("\t- Database already exists!")
+              continue
+            else:
+              # SQL queries
+              sql =  '''
+                CREATE DATABASE {};
+               '''.format(name.lower())  
+              curs.execute(sql)
+              conn.commit()
+              print("\t- Created database: {}".format(name.lower()))
+              conn.close()
+              # connect to new database
+              conn = psycopg2.connect(dbname=name, user=args.U, password=args.w)
+              curs = conn.cursor()
+              print("\t- Connected to {}".format(name.lower()))
+              curs.execute(required_tables_sql)
+              conn.commit()
+              print("\t- Created required tables.")
+              for table in ['agency','calendar','calendar_dates','routes','shapes','stop_times','stops','trips']:
+                with myzip.open('{}.txt'.format(table)) as myfile:
+                  try:
+                    sql = '''COPY {table} FROM STDIN WITH CSV HEADER;'''.format(table = table)
+                    curs.copy_expert(sql, myfile)
+                  except:
+                    if table=='routes':
+                      conn = psycopg2.connect(dbname=name, user=args.U, password=args.w)
+                      curs = conn.cursor()
+                      # known likely issue with legacy feeds, not having route colour variable
+                      # so we try again and specify specific fields in hope it works
+                      sql = '''
+                      COPY {table} 
+                           (route_id,agency_id,route_short_name,route_long_name,route_type) 
+                      FROM STDIN WITH CSV HEADER;
+                      '''.format(table = table)
+                      curs.copy_expert(sql, myfile)
+                    else:
+                        raise
+              print("\t- Updated required tables with data.")
+              sql = '''
+                ALTER TABLE stop_times 
+                    ALTER COLUMN shape_dist_traveled TYPE double precision USING NULLIF(shape_dist_traveled, '')::double precision'''
+              curs.execute(sql)
+              conn.commit()
+              sql = '''CREATE EXTENSION postgis;SELECT postgis_full_version();'''
+              curs.execute(sql)
+              conn.commit()
+              print("\t - Created postgis extension.")
+              conn.close()
 conn.close()
-
-# -- 1. aquire GTFS feeds 
-# --- for the case of PTV (Victoria's public transport agency), these are said to be located at data.vic.gov.au
-# --- in practice, a more convenient and uptodate source is https://transitfeeds.com/
-# --- This not only has the current feeds (presumably pushed by PTV to this repository) but also a comprehensive
-# --- archive of previous feeds.
-# --- This particular script was run using the 13 April 2018 feed at https://transitfeeds.com/p/ptv/497/20180413
-
-# -- 2. summarise GTFS calendar data to get sense of time coverage
-# ---- the aim here is to identify a representative week we might use for analysis.
-# ---- A python script was prepared for this purpose: GTFS_calendarSummary.py, stored in the same directory as 
-# ---- this sql script.
-# ---- Locate this script in the parent folder within which other GTFS folders are located; 
-# ---- the script will then iterate over all descendent folders and summarise any calendar.txt files found.
-# ---- The output for the present case is as follows:
-# ----
-# ------ feed        D:\regional\data\vic_trains_coaches\gtfs_vic_ptv_20180413\calendar.txt
-# ------ upload_date                                           20180413
-# ------ entries                                                    278
-# ------ min_start                                             20180412
-# ------ max_start                                             20180528
-# ------ min_end                                               20180412
-# ------ max_end                                               20180610
-# ------ non_overlap                                                  1
-# ------ dailyFreq                                                 TRUE
-# ------ Summary output to D:\regional\data\vic_trains_coaches\gtfs_calendar_summary_20180419.csv.
-# ----
-# ---- So we see that the calendar 
-# ------ has coverage from 12 April to 10 June 2018
-# ------ however some routes are only relevant on 12 April (ie. max_end), while others may not commence until 28 May.
-
-# -- 3. Create the postgresql database for this particular transit feed
-# -- psql -U postgres -c   "DROP DATABASE gtfs_vic_ptv_20180413;"
-# -- psql -U postgres -c   "CREATE DATABASE gtfs_vic_ptv_20180413;"
-# -- Run the following from within psql
-# ---- this was sourced from https://github.com/chroman/df-gtfs
-# ---- some points, e.g. the creation of a specific wgs84 numeric type may be redundant; but the data
-# ---- checking rule has value (e.g. sometimes coords are swapped, or outright wrong).  This guards
-# ---- against these instances.
-# ---- If applying this script to another feed, the table field names may need to be updated to reflect
-# ---- the headers and datatypes of the supplied data (not always consistent across feeds).
-# DROP DOMAIN IF EXISTS wgs84_lat CASCADE;
-# CREATE DOMAIN wgs84_lat AS DOUBLE PRECISION CHECK(VALUE >= -90 AND VALUE <= 90);
-
-# DROP DOMAIN IF EXISTS wgs84_lon CASCADE;
-# CREATE DOMAIN wgs84_lon AS DOUBLE PRECISION CHECK(VALUE >= -180 AND VALUE <= 180);
-
-# DROP DOMAIN IF EXISTS gtfstime CASCADE;
-# CREATE DOMAIN gtfstime AS text CHECK(VALUE ~ '^[0-9]?[0-9]:[0-5][0-9]:[0-5][0-9]$');
-
-# CREATE TABLE agency
-# (
-  # agency_id         text UNIQUE NULL,
-  # agency_name       text NOT NULL,
-  # agency_url        text NOT NULL,
-  # agency_timezone   text NOT NULL,
-  # agency_lang       text NULL
-# );
-
-# CREATE TABLE calendar
-# (
-  # service_id        text PRIMARY KEY,
-  # monday            boolean NOT NULL,
-  # tuesday           boolean NOT NULL,
-  # wednesday         boolean NOT NULL,
-  # thursday          boolean NOT NULL,
-  # friday            boolean NOT NULL,
-  # saturday          boolean NOT NULL,
-  # sunday            boolean NOT NULL,
-  # start_date        numeric(8) NOT NULL,
-  # end_date          numeric(8) NOT NULL
-# );
-
-# CREATE TABLE calendar_dates
-# (
-  # service_id text NOT NULL,
-  # date numeric(8) NOT NULL,
-  # exception_type integer NOT NULL
-# );
-
-# CREATE TABLE routes
-# (
-  # route_id          text PRIMARY KEY,
-  # agency_id         text NULL,
-  # route_short_name  text NULL,
-  # route_long_name   text NOT NULL,
-  # route_type        integer NULL,
-  # route_color       text NULL,
-  # route_text_color  text NULL
-# );
-
-# CREATE TABLE shapes
-# (
-  # shape_id          text,
-  # shape_pt_lat      wgs84_lat NOT NULL,
-  # shape_pt_lon      wgs84_lon NOT NULL,
-  # shape_pt_sequence integer NOT NULL,
-  # shape_dist_traveled double precision NULL
-# );
-
-# -- CREATE TABLE stop_times
-# -- (
-  # -- trip_id           text NOT NULL,
-  # -- arrival_time      interval NOT NULL,
-  # -- departure_time    interval NOT NULL,
-  # -- stop_id           text NOT NULL,
-  # -- stop_sequence     integer NOT NULL,
-  # -- stop_headsign     text NULL,
-  # -- pickup_type       integer NULL CHECK(pickup_type >= 0 and pickup_type <=3),
-  # -- drop_off_type     integer NULL CHECK(drop_off_type >= 0 and drop_off_type <=3),
-  # -- shape_dist_traveled double precision NULL
-# -- );
-# -- Using a work around due to empty value in shape_dist_traveled field; can't be interpreted as numeric directly
-# CREATE TABLE stop_times_empty_shape_dist
-# (
-  # trip_id           text NOT NULL,
-  # arrival_time      interval NOT NULL,
-  # departure_time    interval NOT NULL,
-  # stop_id           text NOT NULL,
-  # stop_sequence     integer NOT NULL,
-  # stop_headsign     text NULL,
-  # pickup_type       integer NULL CHECK(pickup_type >= 0 and pickup_type <=3),
-  # drop_off_type     integer NULL CHECK(drop_off_type >= 0 and drop_off_type <=3),
-  # shape_dist_traveled text NULL
-# );
-
-# CREATE TABLE stops
-# (
-  # stop_id           text PRIMARY KEY,
-  # stop_name         text NOT NULL,
-  # stop_lat          wgs84_lat NOT NULL,
-  # stop_lon          wgs84_lon NOT NULL
-# );
-
-# CREATE TABLE trips
-# (
-  # route_id          text NOT NULL,
-  # service_id        text NOT NULL,
-  # trip_id           text NOT NULL PRIMARY KEY,
-  # shape_id          text NULL,
-  # trip_headsign     text NULL,
-  # direction_id      boolean NULL
-# );
-
-# -- Copy data into created tables
-# \copy agency from 'D:/regional/data/vic_trains_coaches/gtfs_vic_ptv_20180413/agency.txt' with csv header
-# \copy calendar from 'D:/regional/data/vic_trains_coaches/gtfs_vic_ptv_20180413/calendar.txt' with csv header
-# \copy calendar_dates from 'D:/regional/data/vic_trains_coaches/gtfs_vic_ptv_20180413/calendar_dates.txt' with csv header
-# \copy routes from 'D:/regional/data/vic_trains_coaches/gtfs_vic_ptv_20180413/routes.txt' with csv header
-# \copy shapes from 'D:/regional/data/vic_trains_coaches/gtfs_vic_ptv_20180413/shapes.txt' with csv header
-# \copy stop_times_empty_shape_dist from 'D:/regional/data/vic_trains_coaches/gtfs_vic_ptv_20180413/stop_times.txt' with csv header
-# \copy stops from 'D:/regional/data/vic_trains_coaches/gtfs_vic_ptv_20180413/stops.txt' with csv header
-# \copy trips from 'D:/regional/data/vic_trains_coaches/gtfs_vic_ptv_20180413/trips.txt' with csv header
-
-# CREATE TABLE stop_times AS
- # SELECT trip_id            ,
-        # arrival_time       ,
-        # departure_time     ,
-        # stop_id            ,
-        # stop_sequence      ,
-        # stop_headsign      ,
-        # pickup_type        ,
-        # drop_off_type      ,
-        # NULLIF(shape_dist_traveled, '')::double precision::int
-  # FROM stop_times_empty_shape_dist;
-# -- drop the temporary work around table 
- # DROP TABLE stop_times_empty_shape_dist;  
-  
-# -- enable postgis extension
-# CREATE EXTENSION postgis;SELECT postgis_full_version();  
